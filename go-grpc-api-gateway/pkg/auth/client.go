415b0ef7f03fdb661737bf8d850308324a8cfeb1
Eko Aprili Trisno <eaprilitrisno@gmail.com>
2022-08-19 03:33:22 +0700

    cleanup transport insecure

--- go-grpc-api-gateway/pkg/auth/client.go
+++ go-grpc-api-gateway/pkg/auth/client.go
@@ -6,6 +6,7 @@ import (
 	"github.com/eatrisno/go-grpc-api-gateway/pkg/auth/pb"
 	"github.com/eatrisno/go-grpc-api-gateway/pkg/utils"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type ServiceClient struct {
@@ -14,7 +15,7 @@ type ServiceClient struct {
 
 func InitServiceClient(c *utils.Config) pb.AuthServiceClient {
 	// using WithInsecure() because no SSL running
-	cc, err := grpc.Dial(c.AuthSvcUrl, grpc.WithInsecure())
+	cc, err := grpc.Dial(c.AuthSvcUrl, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
 		fmt.Println("Could not connect:", err)
